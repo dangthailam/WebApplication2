19de8d37e6d94247aa291376e611a3f3e42a726c
DANG Thai Lam <lamtp1989@gmail.com>
2015-07-22 00:21:40 +0200

    CVMaker

---
+++ WebApplication2/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Optimization;
+
+namespace WebApplication2
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            ScriptBundle bootstrapScripts = new ScriptBundle("~/Scripts/jquery-{version}.js");
+
+            bootstrapScripts.Include("~/Scripts/bootstrap.js");
+
+            ScriptBundle angularScripts = new ScriptBundle("~/Scripts/angular.js");
+
+            angularScripts.Include("~/Scripts/angular-route.js",
+                "~/Scripts/angular-ui/ui-bootstrap-tpls.js",
+                "~/Scripts/angular-ui/ui-bootstrap.js");
+
+            StyleBundle bootstrapCss = new StyleBundle("~/Content/bootstrap.css");
+
+            bootstrapCss.Include("~/Content/bootstrap-theme.css",
+                "~/Content/ui-bootstrap-csp.css");
+
+            bundles.Add(bootstrapScripts);
+            bundles.Add(angularScripts);
+            bundles.Add(bootstrapCss);
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
